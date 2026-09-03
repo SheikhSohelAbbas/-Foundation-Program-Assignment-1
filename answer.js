5d835270bf0b487c11b5205ef29c00f409d1f7ab
SheikhSohelAbbas <sheikhsohelabbas@gmail.com>
2026-09-03 19:19:11 +0600

    Added Question 3 solutions

--- answer.js
+++ answer.js
@@ -14,20 +14,34 @@ function describeValue(value) {
 
 function getDayType(day) {
     const dayName = day.toLowerCase();
-
      switch (dayName) {
         case "friday":
         case "saturday":
-            return "Weekend";
+             return "Weekend";
 
         case "sunday":
         case "monday":
         case "tuesday":
         case "wednesday":
         case "thursday":
-            return "Working Day";
+        return "Working Day";
 
         default:
             return "Invalid Day";
     }
+}
+
+// Question 3: Username Gatekeeper 
+
+function validateUsername (username) {
+    if (username.length < 4) {
+        return " Too Short " ;
+    }
+    if (username.includes(" ")) {
+        return " No Space Allowed " ;
+    }
+    if (username.toLowerCase().includes("admin")) {
+        return " Reserved Word ";
+    }
+    return " Available " ;
 }
