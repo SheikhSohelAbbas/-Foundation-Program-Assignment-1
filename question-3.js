bcd3e913551d019d1f31b0d8bfae95bf9e3325c0
SheikhSohelAbbas <sheikhsohelabbas@gmail.com>
2026-09-03 23:50:54 +0600

    Complete

--- question-3.js
+++ question-3.js
@@ -16,9 +16,15 @@ function validateUsername (username) {
 //   Test
 
 // console.log(validateUsername("rahim123"));
+
 // console.log(validateUsername("ab"));
+
 console.log(validateUsername("a b"));
+
 // console.log(validateUsername("abcd"));
+
 // console.log(validateUsername("rahim islam"));
+
 console.log(validateUsername("superadmin99"));
+
 console.log(validateUsername("Admin_Rahim"));
